8368145cda90c01a8ffadb6b5c682fc7c30bcfb1
Zakaria <elismailizakariae@gmail.com>
2021-07-23 12:26:25 +0100

    criation page pour lode view edit

--- app/views/contacts/edit.php
+++ app/views/contacts/edit.php
@@ -5,24 +5,24 @@
     <p>Create a produit with this form</p>
     <form action="<?php echo URLROOT; ?>/contacts/edit/<?php echo $data['id']; ?>" method="post">
       <div class="form-group">
-        <label for="name">Name: <sup>*</sup></label>
-        <input type="text" name="name" class="form-control form-control-lg <?php echo (!empty($data['name_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['name']; ?>">
-        <span class="invalid-feedback"><?php echo $data['name_err']; ?></span>
+        <label for="name">Name produit: <sup>*</sup></label>
+        <input type="text" name="prod_name" class="form-control form-control-lg <?php echo (!empty($data['prod_name_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['prod_name']; ?>">
+        <span class="invalid-feedback"><?php echo $data['prod_name_err']; ?></span>
       </div>
       <div class="form-group">
-        <label for="number">Number: <sup>*</sup></label>
-        <textarea name="number" class="form-control form-control-lg <?php echo (!empty($data['number_err'])) ? 'is-invalid' : ''; ?>"><?php echo $data['number']; ?></textarea>
-        <span class="invalid-feedback"><?php echo $data['number_err']; ?></span>
+        <label for="Prix">Prix: <sup>*</sup></label>
+        <textarea name="prod_prix" class="form-control form-control-lg <?php echo (!empty($data['prod_prix_err'])) ? 'is-invalid' : ''; ?>"><?php echo $data['prod_prix']; ?></textarea>
+        <span class="invalid-feedback"><?php echo $data['prod_prix_err']; ?></span>
       </div>
       <div class="form-group">
-        <label for="email">Email: <sup>*</sup></label>
-        <textarea name="email" class="form-control form-control-lg <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>"><?php echo $data['email']; ?></textarea>
-        <span class="invalid-feedback"><?php echo $data['email_err']; ?></span>
+        <label for="details">Details: <sup>*</sup></label>
+        <textarea name="prod_details" class="form-control form-control-lg <?php echo (!empty($data['prod_details_err'])) ? 'is-invalid' : ''; ?>"><?php echo $data['prod_details']; ?></textarea>
+        <span class="invalid-feedback"><?php echo $data['prod_details_err']; ?></span>
       </div>
       <div class="form-group">
-        <label for="address">Address: <sup>*</sup></label>
-        <textarea name="address" class="form-control form-control-lg <?php echo (!empty($data['address_err'])) ? 'is-invalid' : ''; ?>"><?php echo $data['address']; ?></textarea>
-        <span class="invalid-feedback"><?php echo $data['address_err']; ?></span>
+        <label for="title">title: <sup>*</sup></label>
+        <textarea name="prod_title" class="form-control form-control-lg <?php echo (!empty($data['prod_title_err'])) ? 'is-invalid' : ''; ?>"><?php echo $data['prod_title']; ?></textarea>
+        <span class="invalid-feedback"><?php echo $data['prod_title_err']; ?></span>
       </div>
       <input type="submit" class="btn btn-success" value="Submit">
     </form>
