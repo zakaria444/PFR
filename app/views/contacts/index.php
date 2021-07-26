f82501651495936d54d273724a8121fd3916305b
Zakaria <elismailizakariae@gmail.com>
2021-07-26 15:56:14 +0100

    addinge style grid

--- app/views/contacts/index.php
+++ app/views/contacts/index.php
@@ -1,24 +1,33 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-  <?php flash('contact_message'); ?>
-  <div class="row mb-3">
-    <div class="col-md-6">
-      <h1>Prodact</h1>
-    </div>
-    <div class="col-md-6">
-      <a href="<?php echo URLROOT; ?>/contacts/add" class="btn btn-primary pull-right">
-        <i class="fa fa-pencil"></i> Add Produit
-      </a>
-    </div>
+
+<?php flash('contact_message'); ?>
+<div class="row mb-3">
+  <div class="col-md-6">
+    <h1>Prodact</h1>
   </div>
-  <?php foreach($data['product'] as $contact) : ?>
-    <div class="card card-body mb-3">
+  <div class="col-md-6">
+    <a href="<?php echo URLROOT; ?>/contacts/add" class="btn btn-primary pull-right">
+      <i class="fa fa-pencil"></i> Add Produit
+    </a>
+  </div>
+</div>
+
+<div class="row">
+<?php foreach ($data['product'] as $contact) : ?>
+  <div class=" w-40 col-6 " >
+    <div class="card card-body mb-3 ">
       <h4 class="card-title"><?php echo $contact->prod_name; ?></h4>
       <div class="bg-light p-2 mb-3">
-      
 
-       <img src="data:image/png;base64,<?php echo $contact->img; ?>">
+
+        <img src="data:image/png;base64,<?php echo $contact->img; ?> "  style="width:20%; ">
       </div>
       <a href="<?php echo URLROOT; ?>/contacts/show/<?php echo $contact->id_product; ?>" class="btn btn-dark">More</a>
     </div>
-  <?php endforeach; ?>
+  </div>
+<?php endforeach; ?>
+</div>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
+<style>
+
+</style>
