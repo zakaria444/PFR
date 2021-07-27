e6dd055f8632738695b577a9363de713fc406484
Zakaria <elismailizakariae@gmail.com>
2021-07-27 16:33:21 +0100

    adding section to home page

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -13,9 +13,10 @@
   <div class=" w-40 col-4 " >
     <div class="card card-body mb-3">
       <div class="cont" style="display:flex; ">
-    <div class="title" style="margin-left: 30px;">
-      <h4 class="card-title"  > Very First Sight</h4>
-      <h4 style="border:0.5px solid; width: 50%;background-color: #FFF5EB;text-align: center;">DH</h4>
+    <div class="title" style="    background-color:#DDDDDD;">
+      <h4 class="card-title" style="margin-left: 55px;"  > Very First Sight</h4>
+      <img src="<?php echo URLROOT; ?>/img/1002.jpg" style="width: 60%; margin-left: 55px;" >
+      <h4 style="border:0.5px solid; width: 60%;background-color: #FFF5EB;text-align: center; margin-left: 55px;margin-top: 30px;">49 DH</h4>
       </div>
       </div>
       <div class="bg-light p-2 mb-3">
@@ -27,9 +28,10 @@
   <div class=" w-40 col-4 " >
     <div class="card card-body mb-3">
       <div class="cont" style="display:flex; ">
-    <div class="title" style="margin-left: 30px;">
-      <h4 class="card-title"   > 1001 Activities Book</h4>
-      <h4 style="border:0.5px solid; width: 50%;background-color: #FFF5EB;text-align: center;">DH</h4>
+    <div class="title"  style="    background-color:#DDDDDD;">
+      <h4 class="card-title"  style="margin-left: 55px;"   > 1001 Activities Book</h4>
+      <img src="<?php echo URLROOT; ?>/img/1001.jpg" style="width: 60%; margin-left: 55px;" >
+      <h4 style="border:0.5px solid; width: 60%;background-color: #FFF5EB;text-align: center; margin-left: 55px;margin-top: 30px;"> 59 DH</h4>
       </div>
       </div>
       <div class="bg-light p-2 mb-3">
@@ -40,9 +42,11 @@
     <div class=" w-40 col-4 " >
     <div class="card card-body mb-3">
       <div class="cont" style="display:flex; ">
-    <div class="title" style="margin-left: 30px;">
-      <h4 class="card-title"   > The Illustrated Stories</h4>
-      <h4 style="border:0.5px solid; width: 50%;background-color: #FFF5EB;text-align: center;">DH</h4>
+    <div class="title"  style="    background-color:#DDDDDD;">
+      <h4 class="card-title" style="margin-left: 55px;"   > The Illustrated Stories</h4>
+      <img src="<?php echo URLROOT; ?>/img/1003.jpg" style="width: 60%; margin-left: 55px;" >
+
+      <h4 style="border:0.5px solid; width: 50%;background-color: #FFF5EB;text-align: center; margin-left: 55px;margin-top: 30px;"> 69 DH</h4>
       </div>
       </div>
       <div class="bg-light p-2 mb-3">
